e06d7dfc2e448d0bfcbfcc66f266157ae4020804
jordilaforge <8899ph@web.de>
2015-08-28 18:28:06 +0200

    implemented asynchronous execution of calculation progress needs to be implemented

--- src/main/ImageComparerMain.java
+++ src/main/ImageComparerMain.java
@@ -2,6 +2,7 @@ package main;
 
 import javafx.application.Application;
 import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -27,157 +28,163 @@ import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-public class ImageComparerMain extends Application{
+public class ImageComparerMain extends Application {
 
-	static final int threadnumber=4;
-	//buttons
-	Button btn1;
-	Button btn2;
-	TextField field;
-	Text status;
-	ArrayList<File> allFiles;
-	ScanDirectory scanDirectory;
-	CopyOnWriteArrayList<CompareItem> sameFilesParallelThread;
-	GridPane gridPane;
-	ProgressBar pb;
+    static final int threadnumber = 4;
+    //buttons
+    Button btn1;
+    Button btn2;
+    TextField field;
+    Text status;
+    ArrayList<File> allFiles;
+    ScanDirectory scanDirectory;
+    CopyOnWriteArrayList<CompareItem> sameFilesParallelThread;
+    GridPane gridPane;
+    ProgressBar pb;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception{
-		//Settings
-		primaryStage.setTitle("Image Comparer");
-		//Textfield
-		field = new TextField();
-		field.setText("No Directory Selected");
-		field.setPrefSize(400,20);
-		//Buttons
-		btn1 = new Button();
-		btn1.setText("Directory");
-		btn1.setOnAction(e -> {
-			DirectoryChooser directoryChooser = new DirectoryChooser();
-			File selectedDirectory =
-					directoryChooser.showDialog(primaryStage);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //Settings
+        primaryStage.setTitle("Image Comparer");
+        //Textfield
+        field = new TextField();
+        field.setText("No Directory Selected");
+        field.setPrefSize(400, 20);
+        //Buttons
+        btn1 = new Button();
+        btn1.setText("Directory");
+        btn1.setOnAction(e -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            File selectedDirectory =
+                    directoryChooser.showDialog(primaryStage);
 
-			if (selectedDirectory == null) {
-				System.out.println("No Directory Choosen");
-				;
-			} else {
-				System.out.println(selectedDirectory.getAbsolutePath());
-				field.setText(selectedDirectory.getAbsolutePath());
-				scanDirectory = new ScanDirectory();
-				allFiles= scanDirectory.scanDir(selectedDirectory.getAbsolutePath());
-				status.setText("Directory: "+selectedDirectory.getAbsolutePath()+" Number of files: "+allFiles.size());
-			}
-		});
-		btn2 = new Button();
-		btn2.setText("Search");
-		btn2.setOnAction(e -> {
-			if(allFiles.size()>0){
-				status.setText("Searching...");
-					doComparison(field.getText());
-			}
-			else{
-				status.setText("No Image Files Found!");
-			}
-		});
-		//Hbox
-		HBox hbox = new HBox();
-		hbox.setPadding(new Insets(15, 12, 15, 12));
-		hbox.setSpacing(10);
-		hbox.setStyle("-fx-background-color: #DCF0F7;");
-		hbox.getChildren().addAll(field,btn1,btn2);
-		//imageview
-		gridPane = new GridPane();
-		Text a = new Text("");
-		Text b = new Text("");
-		Text c = new Text("");
-		gridPane.add(a, 0, 0);
-		gridPane.add(b, 1, 0);
-		gridPane.add(c, 2, 0);
-		ScrollPane sp = new ScrollPane();
-		sp.setContent(gridPane);
-		//statusbar
-		HBox hboxStatus = new HBox();
-		hboxStatus.setStyle("-fx-background-color: #DCF0F7;");
-		pb = new ProgressBar(0);
+            if (selectedDirectory == null) {
+                System.out.println("No Directory Choosen");
+                ;
+            } else {
+                System.out.println(selectedDirectory.getAbsolutePath());
+                field.setText(selectedDirectory.getAbsolutePath());
+                scanDirectory = new ScanDirectory();
+                allFiles = scanDirectory.scanDir(selectedDirectory.getAbsolutePath());
+                status.setText("Directory: " + selectedDirectory.getAbsolutePath() + " Number of files: " + allFiles.size());
+            }
+        });
+        btn2 = new Button();
+        btn2.setText("Search");
+        btn2.setOnAction(e -> {
+            if (allFiles.size() > 0) {
+                if(sameFilesParallelThread!=null){
+                    sameFilesParallelThread.clear();
+                    gridPane.getChildren().clear();
+                }
+                status.setText("Searching...");
+                doComparison(field.getText());
+            } else {
+                status.setText("No Image Files Found!");
+            }
+        });
+        //Hbox
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #DCF0F7;");
+        hbox.getChildren().addAll(field, btn1, btn2);
+        //imageview
+        gridPane = new GridPane();
+        ScrollPane sp = new ScrollPane();
+        sp.setContent(gridPane);
+        //statusbar
+        HBox hboxStatus = new HBox();
+        hboxStatus.setStyle("-fx-background-color: #DCF0F7;");
+        pb = new ProgressBar(0);
 
-		status = new Text();
-		status.setText("No Directory Selected...");
-		hboxStatus.getChildren().addAll(pb,status);
-		//borderpane
-		BorderPane border = new BorderPane();
-		border.setTop(hbox);
-		border.setCenter(sp);
-		border.setBottom(hboxStatus);
-		//layout
-		StackPane layout = new StackPane();
-		layout.getChildren().add(border);
-		Scene scene = new Scene(layout,600,275);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+        status = new Text();
+        status.setText("No Directory Selected...");
+        hboxStatus.getChildren().addAll(pb, status);
+        //borderpane
+        BorderPane border = new BorderPane();
+        border.setTop(hbox);
+        border.setCenter(sp);
+        border.setBottom(hboxStatus);
+        //layout
+        StackPane layout = new StackPane();
+        layout.getChildren().add(border);
+        Scene scene = new Scene(layout, 600, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 
-	public void doComparison(String absolutePath){
+    public void doComparison(String absolutePath) {
 
 
+        Task task = new Task<CopyOnWriteArrayList>() {
+            @Override
+            public CopyOnWriteArrayList call() {
+                CopyOnWriteArrayList cowal = scanDirectory.scanForSameParallelThread();
+                updateProgress(1,1);
+                return cowal;
 
-		Task task = new Task<Void>() {
-			@Override
-			public Void call() {
-				sameFilesParallelThread= scanDirectory.scanForSameParallelThread();
-					status.setText("Finished! similar Images: " + sameFilesParallelThread.size());
-					for(int i=0;i<sameFilesParallelThread.size();++i){
-						Image imagel=null;
-						Image imager=null;
-						try {
-							imagel = new Image(String.valueOf(new File(sameFilesParallelThread.get(i).getImage1()).toURI().toURL()));
-							imager = new Image(String.valueOf(new File(sameFilesParallelThread.get(i).getImage2()).toURI().toURL()));
-						} catch (MalformedURLException e) {
-							e.printStackTrace();
-						}
-						ImageView picl = new ImageView();
-						picl.setImage(imagel);
-						picl.setFitHeight(100);
-						picl.setFitWidth(100);
-						VBox vboxPicL = new VBox();
-						Text namel = new Text(sameFilesParallelThread.get(i).getImage1().substring(sameFilesParallelThread.get(i).getImage1().lastIndexOf("\\") + 1));
-						vboxPicL.getChildren().addAll(picl, namel);
-						gridPane.add(vboxPicL, 0, i);
-						Text similarityName = new Text();
-						similarityName.setText("Similarity");
-						Text similarity = new Text(String.valueOf(sameFilesParallelThread.get(i).getSimilarity()) + "%");
-						VBox vboxSimilarity = new VBox();
-						vboxSimilarity.getChildren().addAll(similarityName,similarity);
-						vboxSimilarity.setAlignment(Pos.CENTER);
-						gridPane.add(vboxSimilarity, 1, i);
-						ImageView picr = new ImageView();
-						picr.setImage(imager);
-						picr.setFitHeight(100);
-						picr.setFitWidth(100);
-						VBox vboxPicR = new VBox();
-						Text namer = new Text(sameFilesParallelThread.get(i).getImage2().substring(sameFilesParallelThread.get(i).getImage2().lastIndexOf("\\")+1));
-						vboxPicR.getChildren().addAll(picr, namer);
-						gridPane.add(vboxPicR, 2, i);
-						updateProgress(i,sameFilesParallelThread.size());
-					}
+            }
 
 
-				return null;
-			}
-		};
-		pb.progressProperty().bind(task.progressProperty());
-		new Thread(task).start();
+        };
+        task.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED,
+                new EventHandler<WorkerStateEvent>() {
+                    @Override
+                    public void handle(WorkerStateEvent t) {
+                        sameFilesParallelThread = (CopyOnWriteArrayList<CompareItem>) task.getValue();
+                        for (int i = 0; i < sameFilesParallelThread.size(); ++i) {
+                            Image imagel = null;
+                            Image imager = null;
+                            try {
+                                imagel = new Image(String.valueOf(new File(sameFilesParallelThread.get(i).getImage1()).toURI().toURL()));
+                                imager = new Image(String.valueOf(new File(sameFilesParallelThread.get(i).getImage2()).toURI().toURL()));
+                            } catch (MalformedURLException e) {
+                                e.printStackTrace();
+                            }
+                            ImageView picl = new ImageView();
+                            picl.setImage(imagel);
+                            picl.setFitHeight(100);
+                            picl.setFitWidth(100);
+                            VBox vboxPicL = new VBox();
+                            Text namel = new Text(sameFilesParallelThread.get(i).getImage1().substring(sameFilesParallelThread.get(i).getImage1().lastIndexOf("\\") + 1));
+                            vboxPicL.getChildren().addAll(picl, namel);
+                            gridPane.add(vboxPicL, 0, i);
+                            Text similarityName = new Text();
+                            similarityName.setText("Similarity");
+                            Text similarity = new Text(String.valueOf(sameFilesParallelThread.get(i).getSimilarity()) + "%");
+                            VBox vboxSimilarity = new VBox();
+                            vboxSimilarity.getChildren().addAll(similarityName, similarity);
+                            vboxSimilarity.setAlignment(Pos.CENTER);
+                            gridPane.add(vboxSimilarity, 1, i);
+                            ImageView picr = new ImageView();
+                            picr.setImage(imager);
+                            picr.setFitHeight(100);
+                            picr.setFitWidth(100);
+                            VBox vboxPicR = new VBox();
+                            Text namer = new Text(sameFilesParallelThread.get(i).getImage2().substring(sameFilesParallelThread.get(i).getImage2().lastIndexOf("\\") + 1));
+                            vboxPicR.getChildren().addAll(picr, namer);
+                            gridPane.add(vboxPicR, 2, i);
+                            status.setText("Finished Scanning! Similar Images Found: " + sameFilesParallelThread.size());
+                        }
+                        if(sameFilesParallelThread.size()==0){
+                            status.setText("Finished Scanning! No Similar Images Found!");
+                        }
 
+                    }
+                }
 
-		System.out.println("Finished");
-	}
-
-
+        );
+        pb.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
+        System.out.println("Finished");
+    }
 
 
 }
